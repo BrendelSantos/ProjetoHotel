6481350f878a1a2eaefed844453b2d8510bc84cd
BrendelSantos <brendelsantos@gmail.com>
2015-07-26 21:37:29 -0300

    Commit inicial do projeto. Classes pacoteHospedagem, tipoQuarto, item, historicoAcesso

---
+++ Codigo/ProjetoHotelWeb/Dominio/Interfaces/IPacoteHospedagens.cs
@@ -0,0 +1,24 @@
+﻿using Dominio.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces
+{
+   public interface IPacoteHospedagens : IRepositorio<PacoteHospedagem>
+    {
+        void Cadastrar(PacoteHospedagem pacoteHospedagem);
+        void Atualizar(PacoteHospedagem pacoteHospedagem);
+        void Remover(PacoteHospedagem pacoteHospedagem);
+        ICollection<PacoteHospedagem> ListarReserva();
+        ICollection<PacoteHospedagem> ListarCheckin();
+        ICollection<PacoteHospedagem> ListarCheckout();
+        PacoteHospedagem ResultadoUnico(int idPacoteHospedagem);
+        PacoteHospedagem ResultadoUnicoReserva(int idReserva);
+        PacoteHospedagem ResultadoUnicoCheckin(int idReserva);
+        PacoteHospedagem ResultadoUnicoCheckout(int idReserva);
+
+    }
+}
